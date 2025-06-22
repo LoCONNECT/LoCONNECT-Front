6375e9182db76be9d27536a813b2d84a0c72eb6e
Taetae1123 <xodus2079@naver.com>
2025-06-22 13:01:16 +0900

    Fix: 주석 수정

--- src/features/Login/index.tsx
+++ src/features/Login/index.tsx
@@ -27,7 +27,7 @@ const Login = () => {
         // TODO: 로그인 요청(아이디랑 비밀번호 보냄)
         await axiosInstance.post("/auth/login", values);
 
-        // TODO: 유저 정보 요청(해당 유저의 id, name, 회원유형(role) 보내주기)
+        // TODO: 유저 정보 요청(해당 유저의 정보 보내주기 -> 회원가입 다 하면 필요한거 요청할거니까 아직 하지말기)
         const res = await axiosInstance.get("/auth/user");
 
         // Zustand store에 유저 정보 저장
